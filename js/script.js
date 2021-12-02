205e8e7138676708d5208e31eff85a55293d5554
furvietto <fulvio-94@hotmail.it>
2021-12-02 17:05:19 +0100

    adding bomb

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 // Consegna
-// L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+// Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git). L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
 // con difficoltà 1 => tra 1 e 100
 // con difficoltà 2 => tra 1 e 81
 // con difficoltà 3 => tra 1 e 49
-// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. :bomba:
+// I numeri nella lista delle bombe non possono essere duplicati.
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle. La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti. Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.
+// BONUS:
+// 1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
+// 2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
 // Consigli del giorno: :party_wizard:
-// Facciamo prima la parte grafica a mano e solo dopo passiamo allo script
 // Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
 // Ad esempio:
-// Di cosa ho bisogno per generare i numeri?
-// Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
-// Le validazioni e i controlli possiamo farli in un secondo momento.
+// Di cosa ho bisogno per generare i numeri? Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti. Le validazioni e i controlli possiamo farli anche in un secondo momento.
+// Buon lavoro, ci becchiamo coi tickets dalle 15 alle 18
 
 // ---------!!!!!!!!!!!!------- con function
 
@@ -67,8 +70,24 @@
 
 // --------!!!!!!!!------- normale
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
+    let bomb = []
+
+    for (let i = 0; i < 16; i++) {
+        min = Math.ceil(min);
+        max = Math.floor(max)
+
+        let element = bomb[i];
+        element =  Math.floor(Math.random() * (max - min + 1) ) + min;
+        
+        while (bomb.includes(element)) {
+            element =  Math.floor(Math.random() * (max - min + 1) ) + min;
+        }
+        bomb.push(element)
+    }
+    return bomb
   }
+
+
 
   
 const button = document.querySelector("button");
@@ -78,18 +97,11 @@ const squareCont = document.querySelector(".square-container");
 
 
 button.addEventListener("click" ,function () {
-    // easy
-    let bomb = [];
 
-
-    while (bomb.length < 18) {
-        let numberRand = bomb.push(getRndInteger(1,20));
-        while (bomb.includes(numberRand)) {
-            numberRand = bomb.push(getRndInteger(1,20))
-        }
-    }
+    const easyBomb = getRndInteger(1,100);
+    const mediumBomb = getRndInteger(1,81);
+    const hardBomb = getRndInteger(1,49);
     
-    console.log(bomb);
 
     if (select.value == "easy") {
         squareCont.innerHTML = ""
@@ -100,9 +112,15 @@ button.addEventListener("click" ,function () {
             squareCont.append(div);
             div.append(i + 1);  
             
-            div.addEventListener("click" , function () {
-                if (bomb.includes(i)) {
-                    this.classList.add("bomb")
+             div.addEventListener("click" , function () {
+                if (easyBomb.includes(i + 1)) {
+                    for (let x = 0; x < 100; x++) {
+                        const red = document.querySelectorAll(".easy");
+                        if (easyBomb.includes(x + 1)) {
+                            red[x].classList.add("bomb");                          
+                        }
+                        squareCont.replaceChild(red[x].cloneNode(true), red[x]); 
+                    }
 
                 } else {
                     this.classList.add("color-blue");
@@ -125,7 +143,18 @@ button.addEventListener("click" ,function () {
             div.append(i + 1);  
             
             div.addEventListener("click" , function () {
-                this.classList.add("color-blue")
+                if (mediumBomb.includes(i + 1)) {
+                    for (let x = 0; x < 81; x++) {
+                        const red = document.querySelectorAll(".medium");
+                        if (mediumBomb.includes(x + 1)) {
+                            red[x].classList.add("bomb");                          
+                        }
+                        squareCont.replaceChild(red[x].cloneNode(true), red[x]); 
+                    }
+
+                } else {
+                    this.classList.add("color-blue");
+                }
             })
         }
 
@@ -141,7 +170,18 @@ button.addEventListener("click" ,function () {
             div.append(i + 1);  
             
             div.addEventListener("click" , function () {
-                this.classList.add("color-blue")
+                if (hardBomb.includes(i + 1)) {
+                    for (let x = 0; x < 49; x++) {
+                        const red = document.querySelectorAll(".hard");
+                        if (hardBomb.includes(x + 1)) {
+                            red[x].classList.add("bomb");                          
+                        }
+                        squareCont.replaceChild(red[x].cloneNode(true), red[x]); 
+                    }
+
+                } else {
+                    this.classList.add("color-blue");
+                }
             })
         }
     }
